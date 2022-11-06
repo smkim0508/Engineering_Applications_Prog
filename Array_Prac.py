9f10bcb4bbfdb4ae2ff113c4a9f9701534cdbcba
unknown <chefminkim58@gmail.com>
2022-11-06 13:05:11 -0500

    array practice upload

---
+++ Array_Prac.py
@@ -0,0 +1,12 @@
+heights = []
+
+heights = [5.0, 5.2, 5.1, 5.4, 4.9, 5.3, 4.5, 4.7, 4.9]
+
+print(heights)
+
+arraysize = len(heights)
+print(arraysize)
+
+for i in range (0,arraysize):   
+  height = heights [i]
+  print(height)
