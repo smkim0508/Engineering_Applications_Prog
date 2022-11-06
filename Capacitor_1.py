999c6e1ed3c545e68756d73b9604161281885f3c
unknown <chefminkim58@gmail.com>
2022-11-06 02:39:24 -0500

    capatitor charging simulations updated

---
+++ Capacitor_1.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(5, 2.5))   # fix for the bug in repl.it
+
+Vs = 5
+R = 10e3    # 10 K
+C = 470e-6  # 470 uF
+i = 0
+Vc = 0 
+Tc = 0      # Time Constant (will be determined)
+Vr = 0
+
+class integrator:
+  total = 0
+  def func(self, x):
+    self.total += x
+    return self.total
+
+integrate_i = integrator()
+
+for t in range(0, 20):
+  Vr = Vs - Vc
+  i = Vr/R
+  Vc = 1/C*integrate_i.func(i)
+
+  plt.scatter(t, Vc, s=5, c='b')
+  plt.pause(0.01)  
+  plt.scatter(t, i*10000, s=5, c='r')
+  plt.pause(0.01)
+  
+#print("Time constant is about", Tc, "s")
